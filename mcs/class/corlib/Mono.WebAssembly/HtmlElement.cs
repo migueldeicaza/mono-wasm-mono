cbfaa9d29be1b7484f1453c7c5d98192b51be1bd
Laurent Sansonetti <lrz@hipbyte.com>
2017-10-11 18:24:37 +0200

    add HtmlElement.{Id,TagName,ClassName,Parent,Children}

--- mcs/class/corlib/Mono.WebAssembly/HtmlElement.cs
+++ mcs/class/corlib/Mono.WebAssembly/HtmlElement.cs
@@ -1,10 +1,58 @@
 using System;
+using System.Collections.Generic;
 
 namespace Mono.WebAssembly
 {
     public class HtmlElement : HtmlNode
     {
         public HtmlElement(string expr) : base(expr) {}
+
+        public string Id
+        { 
+            get { 
+                return Invoke("id");
+            }
+        }
+
+        public string TagName
+        { 
+            get { 
+                return Invoke("tagName");
+            }
+        }
+
+        public string ClassName
+        { 
+            get { 
+                return Invoke("className");
+            }
+
+            set {
+                Invoke("className = \"" + value + "\"");
+            }
+        }
+
+        public HtmlElement Parent
+        {
+            get {
+                return new HtmlElement(InvokeExpr("parent"));
+            }
+        }
+
+        public List<HtmlElement> Children
+        {
+            get {
+                var list = new List<HtmlElement>();
+
+                var count = Int32.Parse(Invoke("children.length"));
+
+                for (int i = 0; i < count; i++) {
+                    list.Add(new HtmlElement(InvokeExpr("children[" + i + "]")));
+                }
+
+                return list;
+            }
+        }
 
         public string InnerText
         {
