65c4d5ad9f526faf4cb07e2bec1a20773fb893e4
Laurent Sansonetti <lrz@hipbyte.com>
2017-10-11 18:24:00 +0200

    add HtmlDocument.{DocumentElement,Body}

--- mcs/class/corlib/Mono.WebAssembly/HtmlDocument.cs
+++ mcs/class/corlib/Mono.WebAssembly/HtmlDocument.cs
@@ -13,6 +13,20 @@ namespace Mono.WebAssembly
             }
         }
 
+        public HtmlElement DocumentElement
+        {
+            get {
+                return new HtmlElement(InvokeExpr("documentElement"));
+            }
+        }
+
+        public HtmlElement Body
+        {
+            get {
+                return new HtmlElement(InvokeExpr("body"));
+            }
+        }
+
         public HtmlElement GetElementById(string id)
         {
             return new HtmlElement(InvokeExpr("getElementById(\"" + id + "\")"));
