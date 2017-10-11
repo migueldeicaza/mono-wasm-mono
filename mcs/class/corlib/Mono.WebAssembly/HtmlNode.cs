c48f010381fd5d844240b51bafae179b1420f957
Laurent Sansonetti <lrz@hipbyte.com>
2017-10-11 21:32:43 +0200

    add HtmlNode.{GetElementsByTagName(),GetElementsByClassName()}

--- mcs/class/corlib/Mono.WebAssembly/HtmlNode.cs
+++ mcs/class/corlib/Mono.WebAssembly/HtmlNode.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 namespace Mono.WebAssembly
 {
     public abstract class HtmlNode : Object
     {
         public HtmlNode(string expr) : base(expr) {}
+        public HtmlNode(int reference) : base(reference) {}
+
+        public List<HtmlElement> GetElementsByTagName(string name)
+        {
+            var references = InvokeArray("getElementsByTagName(\"" + name
+                    + "\")", true);
+
+            return HtmlElement.ListFromReferences(references); 
+        }
+
+        public List<HtmlElement> GetElementsByClassName(string name)
+        {
+            var references = InvokeArray("getElementsByClassName(\"" + name
+                    + "\")", true);
+
+            return HtmlElement.ListFromReferences(references); 
+        }
     }
 }
