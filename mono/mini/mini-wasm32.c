0a3e353a720737a7ada45e34062ac5b5869dcc49
Laurent Sansonetti <lrz@hipbyte.com>
2017-10-18 20:06:34 +0200

    create a mono exception if the expression failed to evaluate

--- mono/mini/mini-wasm32.c
+++ mono/mini/mini-wasm32.c
@@ -34,14 +34,27 @@ mono_arch_register_lowlevel_calls (void)
 {
 }
 
-char *mono_wasm_js_eval_imp (char *expr);
+char *mono_wasm_js_eval_imp (const char *expr, int *exception_raised);
 
 MonoString *
 mono_wasm_js_eval (MonoString *expr)
 {
-    char *res = mono_wasm_js_eval_imp (mono_string_to_utf8 (expr));
+    const char *expr_str = mono_string_to_utf8 (expr);
+    int exception_raised = 0;
+    char *res = mono_wasm_js_eval_imp (expr_str, &exception_raised);
     g_assert (res != NULL);
-    MonoString *mono_res = mono_string_new (mono_domain_get (), res);
+
+    MonoString *mono_res = NULL;
+    if (exception_raised == 1) {
+        char msg[1000];
+        snprintf (msg, sizeof (msg),
+                "Given expression `%s' could not be evaluated: %s", expr_str,
+                res);
+        mono_set_pending_exception (mono_get_exception_argument ("expr", msg));
+    }
+    else {
+        mono_res = mono_string_new (mono_domain_get (), res);
+    }
     free (res);
     return mono_res;
 }
